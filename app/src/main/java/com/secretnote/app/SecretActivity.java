0861d5e447c54421f9c13681b2b6ef423d6543f2
roynow <14412030@qq.com>
2026-02-25 14:14:05 +0800

    Fix: Add missing SharedPreferences import

--- app/src/main/java/com/secretnote/app/SecretActivity.java
+++ app/src/main/java/com/secretnote/app/SecretActivity.java
@@ -7,6 +7,7 @@ import android.content.ClipData;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
